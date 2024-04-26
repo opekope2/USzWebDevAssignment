46b211a44552b96c6c4c05556525d5df996e4202
opekope2 <opekope2@gmail.com>
2024-04-26 18:55:23 +0200

    Add login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,41 @@
+import { AuthService } from '../shared/services/auth.service';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatTabsModule } from "@angular/material/tabs"
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTabsModule,
+    MatToolbarModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl()
+  });
 
+  constructor(private authService: AuthService) { }
+
+  async login() {
+    if (!this.form.valid) return;
+
+    const { email, password } = this.form.value;
+    await this.authService.login(email, password);
+    // TODO
+  }
 }
