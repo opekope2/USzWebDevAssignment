93b361147aee18c0edad274596eb784bc3d4b6af
opekope2 <opekope2@gmail.com>
2024-04-27 23:18:25 +0200

    Add document ID to recipe

--- src/app/shared/data/recipe.ts
+++ src/app/shared/data/recipe.ts
@@ -2,6 +2,7 @@ import { Ingredient } from "./ingredient";
 import { Instruction } from "./instruction";
 
 export interface Recipe {
+    id?: string;
     name: string;
     ingredients: Ingredient[];
     instructions: Instruction[];
